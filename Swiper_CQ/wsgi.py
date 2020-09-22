5ad1c03ff9dddd9885ee0b5322ba40492effbf80
CQ52 <CQ52@CQ_HBK@163.com>
2020-09-22 14:31:00 +0800

    完成了项目初始化

---
+++ Swiper_CQ/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Swiper_CQ project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swiper_CQ.settings')
+
+application = get_wsgi_application()
