2920d05e809210e335ca67675dae1bb5a0e17a2e
CQ52 <CQ52@CQ_HBK@163.com>
2020-09-23 11:11:50 +0800

    发送验证码登录

--- user/apis.py
+++ user/apis.py
@@ -1,1 +1,26 @@
 from django.http import JsonResponse
+from user.logics import send_vcode
+
+def fetch_vcode(request):
+    "给用户发送验证码"
+    phonenum=request.GET.get('phonrnum')
+    if send_vcode(phonenum):
+        return JsonResponse({'code':0,'data':None})
+    else:
+        return JsonResponse({'code': 1000, 'data': None})
+
+def submit_vcode(request):
+    return JsonResponse
+
+def show_profiles(request):
+    return JsonResponse
+
+def update_profiles(request):
+    return JsonResponse
+def qn_token(request):
+    return JsonResponse
+
+def fetch_vcode(request):
+    return JsonResponse
+
+
---
+++ user/logics.py
@@ -0,0 +1,37 @@
+import re
+import random
+from django.core.cache import cache
+from libs.sms import send_sms
+
+def is_phonenum(phonennum):
+    '验证是否是一个正确的手机号'
+    if re.match(r'1[356789]\d{9}$',phonennum):
+        return True
+    else:
+        return False
+
+def random_code(length=6):
+    '产生指定长度的验证码'
+    nums=[str(random.randint(0,9)) for i in range(length)]
+    return ''.join(nums)
+
+
+def send_vcode(phonenum):
+    '给用户发送短信验证码'
+    if not is_phonenum(phonenum):
+        return False
+
+    key='Vcode-%s'%phonenum
+    #检查缓存中是否已有验证码，防止用户频繁调用接口
+    if cache.get(key):
+        return True
+
+    #产生随机数
+    vcode=random_code()
+    print('随机码',vcode)
+    cache.set(key,vcode,600)  #将验证码添加到缓存，并多设置一些时间
+
+    return send_sms(phonenum,vcode)  #向用户手机发送验证码
+
+    #
+
